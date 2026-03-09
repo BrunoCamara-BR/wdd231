7c6aa39f81c0de27fb2c04ead467b0f758a18063
BrunoCamara-BR <bsantos4@byupathway.edu>
2026-03-08 22:41:35 -0300

    w1 wdd 231 assigment completed fix 1

--- scripts/courses.js
+++ scripts/courses.js
@@ -2,20 +2,89 @@ const courseList = document.getElementById("courses");
 const filter = document.querySelector("#filter");
 const coursesInfo = document.querySelector("#courses-info");
 
+// const courses = [
+//   { code: "CSE 110", status: "completed" },
+//   { code: "CSE 111", status: "completed" },
+//   { code: "CSE 210", status: "in-progress" },
+//   { code: "WDD 130", status: "completed" },
+//   { code: "WDD 131", status: "completed" },
+//   { code: "WDD 231", status: "in-progress" },
+// ];
+
 const courses = [
-  { code: "CSE 110", status: "completed" },
-  { code: "CSE 111", status: "completed" },
-  { code: "CSE 210", status: "in-progress" },
-  { code: "WDD 130", status: "completed" },
-  { code: "WDD 131", status: "completed" },
-  { code: "WDD 231", status: "in-progress" },
+  {
+    subject: "CSE",
+    number: 110,
+    title: "Introduction to Programming",
+    credits: 2,
+    certificate: "Web and Computer Programming",
+    description:
+      "This course will introduce students to programming. It will introduce the building blocks of programming languages (variables, decisions, calculations, loops, array, and input/output) and use them to solve problems.",
+    technology: ["Python"],
+    completed: true,
+  },
+  {
+    subject: "WDD",
+    number: 130,
+    title: "Web Fundamentals",
+    credits: 2,
+    certificate: "Web and Computer Programming",
+    description:
+      "This course introduces students to the World Wide Web and to careers in web site design and development. The course is hands on with students actually participating in simple web designs and programming. It is anticipated that students who complete this course will understand the fields of web design and development and will have a good idea if they want to pursue this degree as a major.",
+    technology: ["HTML", "CSS"],
+    completed: true,
+  },
+  {
+    subject: "CSE",
+    number: 111,
+    title: "Programming with Functions",
+    credits: 2,
+    certificate: "Web and Computer Programming",
+    description:
+      "CSE 111 students become more organized, efficient, and powerful computer programmers by learning to research and call functions written by others; to write, call , debug, and test their own functions; and to handle errors within functions. CSE 111 students write programs with functions to solve problems in many disciplines, including business, physical science, human performance, and humanities.",
+    technology: ["Python"],
+    completed: true,
+  },
+  {
+    subject: "CSE",
+    number: 210,
+    title: "Programming with Classes",
+    credits: 2,
+    certificate: "Web and Computer Programming",
+    description:
+      "This course will introduce the notion of classes and objects. It will present encapsulation at a conceptual level. It will also work with inheritance and polymorphism.",
+    technology: ["C#"],
+    completed: false,
+  },
+  {
+    subject: "WDD",
+    number: 131,
+    title: "Dynamic Web Fundamentals",
+    credits: 2,
+    certificate: "Web and Computer Programming",
+    description:
+      "This course builds on prior experience in Web Fundamentals and programming. Students will learn to create dynamic websites that use JavaScript to respond to events, update content, and create responsive user experiences.",
+    technology: ["HTML", "CSS", "JavaScript"],
+    completed: true,
+  },
+  {
+    subject: "WDD",
+    number: 231,
+    title: "Frontend Web Development I",
+    credits: 2,
+    certificate: "Web and Computer Programming",
+    description:
+      "This course builds on prior experience with Dynamic Web Fundamentals and programming. Students will focus on user experience, accessibility, compliance, performance optimization, and basic API usage.",
+    technology: ["HTML", "CSS", "JavaScript"],
+    completed: false,
+  },
 ];
 
 function showList(list) {
   const allList = list.reduce((total, item) => {
     return (
       total +
-      `<li>${item.code}${item.status == "in-progress" ? " - <span>in progress</span>" : ""}</li>`
+      `<li>${item.completed ? "<strong>✓</strong> " : ""}${item.subject} ${item.number}${item.completed ? "" : " - <span>in progress</span>"}</li>`
     );
   }, "");
   courseList.innerHTML = allList;
@@ -27,10 +96,14 @@ filter.addEventListener("submit", (e) => {
   const filter = e.submitter.value;
 
   const filteredList = courses.filter((item) => {
-    return item.code.includes(filter);
+    return item.subject.includes(filter);
   });
 
   showList(filteredList);
 
-  coursesInfo.innerHTML = `The total credits for course listed above <strong>${filteredList.length * 2}</strong>.`;
+  const CreditTotal = filteredList.reduce((total, item) => {
+    return (total += item.credits);
+  }, 0);
+
+  coursesInfo.innerHTML = `The total credits for course listed above <strong>${CreditTotal}</strong>.`;
 });
